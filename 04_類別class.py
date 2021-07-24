be7665b2b3a94ad5ace55cc30b1f86670e09a15f
Yang-Bin Fan <42027199+YangBinFan@users.noreply.github.com>
2021-07-24 15:43:22 +0800

    Update 04_類別class.py

--- 04_類別class.py
+++ 04_類別class.py
@@ -9,9 +9,13 @@ class Man:
 
     def hello(self):
         print("Hello " + self.name + "!")
+    
+    def thank(self):
+        print(self.name + "says /'thank you!/'")
 
     def goodbye(self):
         print("Good-bye " + self.name + "!")
+        
 
 m = Man("Dell")
 m.hello()
